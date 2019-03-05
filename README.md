5c051e9c9d2b47366d1b1e10a6dab5a1017529cf
Cecilia Avery <cecilia@Cecilias-MacBook-Pro.local>
2019-03-05 16:31:24 -0500

    fetch current and five day data

--- README.md
+++ README.md
@@ -6,3 +6,17 @@ Re-write of app in plain javascript because why not
 your thought process when creating the solution.
 any tradeoffs you made.
 anything you might implement with more time (features, fixes, technical debt corrections etc). -->
+
+Would normally put apikey in a git ignore file
+Wanted to re-write the app in vanilla javascript to focus on what I learned with the language
+ - using ternary operators
+ - focus on smaller functions, better variable descriptions, reuseability of code
+ - maybe use bootstrap for styling and css transitions (learned at buildit)
+ - sterize zipcode input
+ - traverse the DOM
+ - object destructuring galore 
+
+More conscientious of UX:
+ Add error handling
+ Add loading animation
+ Performance - way to load api response faster??
--- main.js
+++ main.js
@@ -1,44 +1,107 @@
-window.onload = function() {
+window.onload = function () {
   getWeather()
 }
 
 const apiKey = '3317ddd89ed0eaa04733ad6ab3569291'
-
+let today = new Date()
 let unit = 'imperial'
-let day = 'night' // use ternary to set to 'night' or '' based on time
-let city = document.getElementById('location')
-let date = document.getElementById('date')
-let temp = document.getElementById('temp')
-let desc = document.getElementById('weather-desc')
-let icon = document.getElementById('icon')
-let unitId = document.getElementById('unit')
+
+const daysOfWeek = ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat']
+
+const docMainBody = {
+  city: document.getElementById('location'),
+  date: document.getElementById('date'),
+  temp: document.getElementById('temp'),
+  desc: document.getElementById('weather-desc'),
+  icon: document.getElementById('icon'),
+  unitId: document.getElementById('unit')
+}
 
 getWeather = () => {
   if ("geolocation" in navigator) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      fetchWeather(position.coords.latitude, position.coords.longitude);
+    navigator.geolocation.getCurrentPosition(function (position) {
+      fetchCurrentWeather(position.coords.latitude, position.coords.longitude)
+      fetchForecastWeather(position.coords.latitude, position.coords.longitude)
     })
   }
 }
 
-fetchWeather = (lat, lon) => {
+fetchCurrentWeather = (lat, lon) => {
   fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=${unit}&appid=${apiKey}`)
     .then(response => {
       return response.json()
     })
     .then(myJson => {
       const cityJson = myJson.name
-      const tempJson = Math.round(myJson.main.temp)
-      const weatherJson = myJson.weather[0] //array
-      setMainBody(cityJson, tempJson, weatherJson)
+      const tempJson = myJson.main.temp
+      const weatherJson = myJson.weather[0]
+      const sunlight = {}
+      sunlight.sunrise = myJson.sys.sunrise
+      sunlight.sunset = myJson.sys.sunset
+      setTemp(docMainBody, { cityJson, tempJson, weatherJson, sunlight })
     })
 }
 
-setMainBody = (cityJson, tempJson, weatherJson) => {
-  date.innerText = new Date().toDateString()
-  city.innerText = cityJson
-  temp.innerText = tempJson
-  desc.innerText = weatherJson.description
-  icon.className = `wi-owm-${weatherJson.id}`
-  unitId.innerText = 'imperial' ? 'º F' : 'º C'
+fetchForecastWeather = (lat, lon) => {
+  fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=${unit}&appid=${apiKey}`)
+    .then(response => {
+      return response.json()
+    })
+    .then(myJson => {
+      parseFiveDay(myJson.list)
+    })
+}
+
+setTemp = (doc, { cityJson, tempJson, weatherJson, sunlight, dayOfWeek }) => {
+  let day
+  sunlight ? (day = (today.getTime() > sunlight.sunrise) && (today.getTime() > sunlight.sunset) ? 'day' : 'night') : day = null
+  doc.date.innerText = sunlight ? today.toDateString() : daysOfWeek[new Date(dayOfWeek * 1000).getDay()]
+  doc.city.innerText = typeof cityJson !== 'undefined' ? cityJson : null
+  doc.temp.innerText = Math.round(tempJson)
+  doc.desc.innerText = weatherJson.description
+  doc.icon.className = `wi wi-owm-${day ? day + '-' : ''}${weatherJson.id}`
+  doc.unitId.innerText = 'imperial' ? 'º F' : 'º C'
+}
+
+/* 5-DAY FORECAST */
+parseFiveDay = (arr) => {
+  let fiveDays = arr.filter(forecast => {
+    return (today.getDate() !== new Date(forecast.dt * 1000).getDate()) && (new Date(forecast.dt_txt).getHours() === 9)
+  })
+  mapFiveDay(fiveDays)
+}
+
+mapFiveDay = (arr) => {
+  const forecastNode = document.getElementById('five-day')
+  arr.forEach(day => {
+    let node = document.createElement('div')
+    let dayNode = document.createElement('h6')
+    dayNode.id = 'date-forecast'
+    node.appendChild(dayNode)
+    let descNode = document.createElement('div')
+    descNode.id = 'weather-desc-forecast'
+    node.appendChild(descNode)
+    let iconElem = document.createElement('icon')
+    iconElem.id = 'icon-forecast'
+    node.appendChild(iconElem)
+    let tempNode = document.createElement('div')
+    tempNode.id = 'temp-forecast'
+    node.appendChild(tempNode)
+    let unitNode = document.createElement('div')
+    unitNode.id = 'unit-forecast'
+    node.appendChild(unitNode)
+    forecastNode.appendChild(node)
+    accessFiveDay(node, day)
+  })
+}
+
+accessFiveDay = (node, data) => {
+  let docForecast = {
+    date: node.childNodes[0],
+    desc: node.childNodes[1],
+    icon: node.childNodes[2],
+    temp: node.childNodes[3],
+    unitId: node.childNodes[4]
+  }
+  setTemp(docForecast, { tempJson: data.main.temp, weatherJson: data.weather[0], dayOfWeek: data.dt })
 }
